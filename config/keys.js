52bd80c3772a3bf9c028605d3053ebbe39abbf65
Edoardo Riggio <edo.riggio19@gmail.com>
2020-11-14 12:14:48 +0100

    added .env variables

--- config/keys.js
+++ config/keys.js
@@ -1,11 +1,16 @@
+require('dotenv').config();
+
 // Database name
-const DB_NAME = 'checqme'
+const DB_NAME = process.env.DB_NAME
 
 // Database Password
-const DB_PASS = 'URZ19FMeEbwGaMGU'
+const DB_PASS = process.env.DB_PASS
 
 dbPassword = 'mongodb+srv://checqme:'+ encodeURIComponent(DB_PASS) + `@checqme.wynfg.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
 
 module.exports = {
     mongoURI: dbPassword
 };
+
+console.log('Now the value for DB_NAME is:', DB_NAME);
+console.log('Now the value for DB_PASS is:', DB_PASS);
