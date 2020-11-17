8fd5fce5fd264880d5bc38d0359fed202bda0fb0
Jacopo Di Ventura <jacopo.diventura@gmail.com>
2020-11-17 15:58:53 +0100

    classroom join with link generation and sharing

--- routes/classroom.js
+++ routes/classroom.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const crypto = require("crypto");
+const ObjectId = require("mongodb").ObjectId;
 const router = express.Router();
 const {
   ensureAuthenticated,
@@ -12,47 +14,45 @@ const MasteryCheck = require("../models/MasteryCheck");
 const Classroom = require("../models/Classroom");
 const User = require("../models/User");
 const Topic = require("../models/Topic");
+const TokenClassroom = require("../models/TokenClassroom");
 
 module.exports = router;
-
-
-
 
 // if PROFESSOR/TA = Get all the classrooms where the current user is the professor
 // if STUDENT = returns all classes in which the student is enrolled
 router.get("/", ensureAuthenticated, (req, res) => {
-  if(req.user.role == 1 || req.user.role == 0){
-      Classroom.find({ lecturer: req.user })
-    .populate("topics")
-    .then((result) => {
-      res.json(result);
-    }).catch(err => {
-      console.log(err);
-      res.json({})
-    })
-  } else if (req.user.role == 2){
-    User.findOne({ _id: req.user._id })
-    .then((user) => {
-      if (user) {
-        Classroom.find({ _id: { $in: user.classrooms } })
-          .then((classrooms) => {
-            res.json(classrooms);
-          })
-          .catch((err) => {
-            console.log(err);
-            res.json({});
-          });
-      } else {
+  if (req.user.role == 1 || req.user.role == 0) {
+    Classroom.find({ lecturer: req.user })
+      .populate("topics")
+      .then((result) => {
+        res.json(result);
+      })
+      .catch((err) => {
+        console.log(err);
         res.json({});
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-      res.json({});
-    });
+      });
+  } else if (req.user.role == 2) {
+    User.findOne({ _id: req.user._id })
+      .then((user) => {
+        if (user) {
+          Classroom.find({ _id: { $in: user.classrooms } })
+            .then((classrooms) => {
+              res.json(classrooms);
+            })
+            .catch((err) => {
+              console.log(err);
+              res.json({});
+            });
+        } else {
+          res.json({});
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        res.json({});
+      });
   }
-})
-
+});
 
 /*
 PROFESSOR ROUTES
@@ -78,42 +78,75 @@ router.post("/new", ensureAuthenticated, ensureProfessor, (req, res) => {
   });
 });
 
-
 //create a new invite link
-router.post("/invite/:classroom_id", ensureAuthenticated, ensureProfessor, (req, res)=>{
-  Classroom.findById(req.params.classroom_id).then(c => {
-    if(c){
-      let token = new TokenClassroom({
-        _classId: c._id,
-        token: crypto.randomBytes(20).toString("hex"),
-      });
-      token.save().then(()=>{
-        res.json(`http://${req.headers.host}/user/verify/${token.token}`);
-      }).catch((err)=> {console.log(err);res.status(400)})
-    }else {
-      res.status(400);
-    }
-  });
-})
+router.get(
+  "/invite/:classroom_id",
+  ensureAuthenticated,
+  ensureProfessor,
+  (req, res) => {
+    Classroom.findById(req.params.classroom_id).then((c) => {
+      // console.log(c.lecturer.toString() == req.user._id.toString());
+      if (c && c.lecturer.toString() == req.user._id.toString()) {
+        let token = new TokenClassroom({
+          _classroomId: c._id,
+          token: crypto.randomBytes(20).toString("hex"),
+        });
+        token
+          .save()
+          .then(() => {
+            res.json(
+              `http://${req.headers.host}/classroom/join/${token.token}`
+            );
+          })
+          .catch((err) => {
+            console.log(err);
+            res.status(400).end();
+          });
+      } else {
+        res.status(400).end();
+      }
+    });
+  }
+);
 
 /*
 STUDENT ROUTES
 */
 
-
 //this link will be given to the new students, once clicked it will automatically join the classroom
 //this route CANNOT be used as an API to interact with the database from ajax.
 router.get("/join/:token", ensureAuthenticated, ensureStudent, (req, res) => {
-  TokenClassroom.find({token: req.params.token}).then((t)=>{
-    User.findById(req.user._id).then(u => {
-      u.classrooms.push(t._classroomId);
-      u.save().then(err=>{
-        if(err){
+  // console.log("here")
+  TokenClassroom.findOne({ token: req.params.token })
+    .then((t) => {
+      User.findById(req.user._id)
+        .then((u) => {
+          if(u.classrooms.includes(t.classroom_id)){
+            res.status(400).send("You already joined this class").end();
+          } else {
+            u.classrooms.addToSet(t._classroomId);
+            u.save()
+              .then((new_u) => {
+                if (new_u) {
+                  res.redirect("/dashboard");
+                } else {
+                  console.log("error modifying user: " + u._id);
+                  res.status(400).send("error joining class, retry").end();
+                }
+              })
+              .catch((err) => {
+                console.log(err);
+                res.send("error joining class, retry").end();
+              });
+          }
+        })
+        .catch((err) => {
           console.log(err);
-        }else {
-          res.redirect('/dashboard');
-        }
-      })
-    }).catch(err => {console.log(err); res.send("error joining class, retry")})
-  }).catch(err => {console.log(err); res.send("error joining class, retry")})
-})
+          res.send("error joining class, retry").end();
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.send("error joining class, retry").end();
+    });
+});
