51bcbd277699d24aa9ae4100f8e2afc4d5707b31
Michele Cattaneo <michelecattaneo18@gmail.com>
2020-11-24 17:55:43 +0100

    possible to move through all questions, where the editor changes the syntax highlight

--- public/js/ajax_answer_question.js
+++ public/js/ajax_answer_question.js
@@ -1,5 +1,6 @@
 var questions = undefined;
 var i = 0;
+var editor = undefined;
 
 function init_answer_question() {
     
@@ -13,27 +14,67 @@ function init_answer_question() {
 
         if (questions && questions.length > 0) {
             set_question(questions[0])
+            handle_next_button()
+            init_editor(questions[0]);
         }
+
     })
 }
 
 function set_question(q) {
     
     let section = document.getElementById("options_section");
-    //console.log(q)
+    section.innerHTML = "";
+
+    // Show input buttons
     for (let i=0; i< q.answer.length; ++i) {
         console.log(q.answer[i]);
-        new_input = document.createElement("input");
-        new_input.type = "text";
-        new_input.value = q.answer[i][0];
+        new_input = document.createElement("label");
+        
+        new_input.innerHTML = q.answer[i][0];
 
         new_checkbox = document.createElement("input");
         new_checkbox.type = "checkbox";
         section.appendChild(new_input);
         section.appendChild(new_checkbox);
     }
+
+    // Set next button
+    let btn = document.getElementById("next_question");
+    btn.value = ( (++i) % questions.length );
+    console.log(btn.value);
 }
 
+function handle_next_button() {
+    let button = document.getElementById("next_question");
+    button.addEventListener("click", (e)=> {
+        
+        let value = button.value;
+        set_question(questions[value]);
+        update_editor(questions[value]);
+    })
+}
+
+function init_editor(first_q) {
+    let txt_area = document.getElementById("text_area");
+    let lang = first_q.lang;
+    console.log(lang);
+
+    editor = CodeMirror.fromTextArea(txt_area, {
+        lineNumbers: true,
+        mode: lang,
+        theme: "dracula",
+        readOnly: true,
+        tabSize: 4
+    });
+
+    editor.setValue(first_q.text);
+}
+
+function update_editor(q) {
+    editor.setValue(q.text);
+    editor.setOption("mode", q.lang);
+}
 
 // DB comunication
 
