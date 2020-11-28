5947ca8c463975660e34b2b223a4d94c7efcbdc3
Jacopo Di Ventura <jacopo.diventura@gmail.com>
2020-11-28 12:36:53 +0100

    started appintment reout

---
+++ routes/appointment.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const moment = require("moment");
+const router = express.Router();
+const {
+  ensureAuthenticated,
+  ensureProfessor,
+  ensureStudent,
+  ensureTa,
+} = require("../config/auth");
+
+const User = require("../models/User");
+const MasteryCheck = require("../models/MasteryCheck");
+const ClassroomMasteryDay = require("../models/ClassroomMasteryDay");
+const Availability = require("../models/Availability");
+
+
+router.post("/", ensureAuthenticated, (req, res) => {
+    
+})
+
+module.exports = router;
