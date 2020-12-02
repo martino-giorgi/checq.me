41ea2b16f06e2865ba0d40d26a8ee1d7487337c4
Michele Cattaneo <michelecattaneo18@gmail.com>
2020-12-02 19:06:04 +0100

    added docs

--- public/js/ajax_classroom_manager.js
+++ public/js/ajax_classroom_manager.js
@@ -22,10 +22,17 @@ function display_class_info() {
   document.getElementById("class_info").innerHTML = ejs.views_manager_partial_class_info(API.class_obj);
 }
 
+/**
+ * Render the view of the modal for the user list
+ */
 function render_user_modal() {
   document.getElementById("user-modal-body").innerHTML = ejs.views_manager_partial_class_student_list(API.class_obj);
 }
 
+/**
+ * Set the value of the id of the selected user when clicking on the actions on the user list modal
+ * @param {Number} user_id the id of the selected partecipant
+ */
 function setUser(user_id) {
   selected_user = user_id;
   document.getElementById("toggleTA_btn")
@@ -34,11 +41,22 @@ function setUser(user_id) {
 }
 
 
-// get selected user id
+/**
+ * Get the id of the selected user from the user list modal
+ * @returns {Number} the id of the selected user
+ */
 function getUser() {
   return selected_user;
 }
 
+/**
+ * Make the user with the given id a TA if it's a student, or make it a student if it is a TA.
+ * If it's a student, change the role from 2 to 1, move it from the list of partecipants to the list of teaching assistants
+ * and finally add this classroom in the list of the classrooms for which the user is a TA.
+ * If it's a TA, change the role from 1 to 2 only if it is not TA for another class and move it from the list of teaching assistants
+ * to the list of partecipants. Finally remove the class from the list of the classrooms for which the user is a TA.
+ * @param {ObjectID} user_id The id of the user to be made TA or made back to student
+ */
 function toggleTA(user_id) {
   let body = {
     classroom_id: c_id,
@@ -69,6 +87,11 @@ function toggleTA(user_id) {
 
 }
 
+/**
+ * If the User is a student, make it a Professor. The user must already have the role 0 ( being signed in as a professor ).
+ * If the user is a professor for this class, make it a student for this class.
+ * @param {ObjectID} user_id the ID corresponding to the selected user.
+ */
 function toggleProf(user_id) {
   let body = {
     professor_id: user_id
@@ -97,6 +120,13 @@ function toggleProf(user_id) {
   }
 }
 
+/**
+ * Try to remove a partecipant from the classroom. The owner of the classroom can not be removed.
+ * Only students can be directly removed. To remove TAs and Professors ( non-owner ) one must first change their role
+ * to student.
+ * On failuer a flash is shown on the client, stating the error.
+ * @param {OnjectID} user_id the selected user.
+ */
 function removeFromClass(user_id) {
   let body = {
     student_id: user_id
@@ -156,6 +186,11 @@ API = (function () {
     });
   }
 
+  /**
+   * Ask the server to make the user a TA for this classroom.
+   * @param {Object} body the body containing the ID of the user.
+   * @returns {Promise} the promise with the status code of the request.
+   */
   function makeTa(body) {
     return fetch("/classroom/ta?classroom_id=" + c_id, {
       method: "POST",
@@ -164,6 +199,11 @@ API = (function () {
     });
   }
 
+  /**
+   * Ask the server to remove the role of TA for this classroom.
+   * @param {Object} body the body containing the ID of the user.
+   * @returns {Promise} the promise with the status code of the request
+   */
   function removeTa(body) {
     return fetch("/classroom/ta?classroom_id=" + c_id, {
       method: "DELETE",
@@ -172,6 +212,11 @@ API = (function () {
     });
   }
 
+  /**
+   * Ask the server to make the user a Professor for this classroom.
+   * @param {Objedt} body the body containing the ID of the user.
+   * @returns {Promise} the promise with the status code of the request
+   */
   function makeProf(body) {
     return fetch("/classroom/professor?classroom_id=" + c_id, {
       method: "POST",
@@ -180,6 +225,11 @@ API = (function () {
     });
   }
 
+  /**
+   * Ask the server to remove the professor role of the user for this classroom.
+   * @param {Onject} body the body containing the ID of the user.
+   * @returns {Promise} the promise with the status code of the request
+   */
   function removeProf(body) {
     return fetch("/classroom/professor?classroom_id=" + c_id, {
       method: "DELETE",
@@ -188,6 +238,11 @@ API = (function () {
     });
   }
 
+  /**
+   * Ask the server to remove the user from the classroom.
+   * @param {Object} body the body containing the ID of the user.
+   * @returns {Promise} the promise with the status code of the request
+   */
   function removeFromClass(body) {
     return fetch("/classroom/student?classroom_id=" + c_id, {
       method: "DELETE",
