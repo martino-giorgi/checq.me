bc28070f883edcd7b0b78c25961d7c7f157afb0a
Edoardo Riggio <edo.riggio19@gmail.com>
2020-12-08 19:31:00 +0100

    the nightmare never ends

--- routes/manager.js
+++ routes/manager.js
@@ -1,13 +1,6 @@
 const express = require("express");
 const router = express.Router();
-<<<<<<< HEAD
-const { ensureAuthenticated,
-        ensureProfOrTA,
-        ensureProfOrTAUser
-      } = require("../config/auth");
-=======
 const { ensureAuthenticated, ensureProfOrTA, ensureProfessorUser, ensureProfOrTAUser } = require("../config/auth");
->>>>>>> main
 const Classroom = require("../models/Classroom");
 const User = require("../models/User");
 
@@ -15,20 +8,6 @@ const User = require("../models/User");
 module.exports = router;
 
 router.get("/", ensureAuthenticated, ensureProfOrTAUser, (req, res) => {
-<<<<<<< HEAD
-  if (req.user.role == 0) {
-    Classroom.find({ lecturer: req.user._id })
-      .populate({
-        path: "teaching_assistants",
-        select: ["name", "surname"],
-      })
-      .then((classrooms) => {
-        res.render("manager/manager", { user: req.user, classrooms: classrooms });
-      });
-  } else if (req.user.role == 1) {
-
-  }
-=======
   Classroom.find({ $or: [{ professors: { $in: [req.user] } }, { teaching_assistants: { $in: [req.user] } }] })
     .populate({
       path: "teaching_assistants",
@@ -38,7 +17,6 @@ router.get("/", ensureAuthenticated, ensureProfOrTAUser, (req, res) => {
       // console.log(classrooms);
       res.render("manager/manager", { user: req.user, classrooms: classrooms });
     });
->>>>>>> main
 });
 
 router.get("/classroom", ensureAuthenticated, ensureProfOrTA, (req, res) => {
