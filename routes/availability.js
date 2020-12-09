08e52e6ea9ef2261d5d0b3841e4e7aad1d92e6db
Jacopo Di Ventura <jacopo.diventura@gmail.com>
2020-12-09 21:43:55 +0100

    started patch busy day

--- routes/availability.js
+++ routes/availability.js
@@ -35,63 +35,63 @@ router.post("/", ensureAuthenticated, ensureProfOrTAUser, async (req, res) => {
   let start = moment(req.body.busy[0], "YYYY-MM-DDTHH:mm:ssZ");
   let end = moment(req.body.busy[1], "YYYY-MM-DDTHH:mm:ssZ");
 
-  let appointments = (await Appointment.find({
-    _taId: req.user._id,
-    // start_time: { 
-    //   $or : [{$lte: start.format()},{}]
-    // },
-    // end_time: { $gt: end.format() },
-    $or : [{start_time: {$lte: end.toDate()}}, 
-          {$and: [{start_time: {$gte: start.toDate()}},{end_time: {$lte: end.toDate()}}]}, 
-          {end_time: {$gte: end.toDate()}}]
-  }))//.map((el)=> {return [start_time, end_time]});
-
-  console.log(appointments);
-
-
-
   let now = moment();
   if (!(start.isValid && end.isValid) || end.diff(now) <= 0 || start.diff(now) <= 0) {
-    ok = false;
-  } else {
-    new_range = start.twix(end);
+    res.status(400).send("Dates are invalid or are in the past");
+    return;
   }
 
-  if (ok) {
-    Availability.findOne({ _userId: req.user._id }).then((avail) => {
-      if (avail) {
-        for (let i = 0; i < avail.busy.length; i++) {
-          let s = moment(avail.busy[i][0]).twix(moment(avail.busy[i][1]));
-          if (
-            new_range.overlaps(s) ||
-            new_range.engulfs(s) ||
-            new_range.equals(s) ||
-            new_range.contains(s)
-          ) {
-            res
-              .status(400)
-              .send("Make sure that the dates do not overlap existing events in the calendar");
-            return;
-          }
-        }
-        let test = [[start, end]];
-        avail.updateOne({ $addToSet: { busy: { $each: test } } }).then((response) => {
-          res.status(200).end();
-        });
-      } else {
-        console.log(
-          "Professor: " +
-            req.user._id +
-            " should have an availability document linked to his account!"
-        );
-        res.status(400).end();
-      }
-    });
-  } else {
-    res.status(400).send("Dates are invalid or are in the past");
+  let appointments = await Appointment.find({
+    _taId: req.user._id,
+    $or: [
+      { start_time: { $lte: end.toDate() } },
+      { $and: [{ start_time: { $gte: start.toDate() } }, { end_time: { $lte: end.toDate() } }] },
+      { end_time: { $gte: end.toDate() } },
+    ],
+  });
+
+  if (appointments.length > 0) {
+    res.status(400).send("Make sure that the dates do not overlap existing events in the calendar");
+    return;
   }
+  new_range = start.twix(end);
+
+  Availability.findOne({ _userId: req.user._id }).then((avail) => {
+    if (avail) {
+      for (let i = 0; i < avail.busy.length; i++) {
+        let s = moment(avail.busy[i][0]).twix(moment(avail.busy[i][1]));
+        if (
+          new_range.overlaps(s) ||
+          new_range.engulfs(s) ||
+          new_range.equals(s) ||
+          new_range.contains(s)
+        ) {
+          res
+            .status(400)
+            .send("Make sure that the dates do not overlap existing events in the calendar");
+          return;
+        }
+      }
+      let test = [[start, end]];
+      avail.updateOne({ $addToSet: { busy: { $each: test } } }).then((response) => {
+        res.status(200).end();
+      });
+    } else {
+      console.log(
+        "Professor/TA: " +
+          req.user._id +
+          " should have an availability document linked to his account!"
+      );
+      res.status(400).end();
+      return;
+    }
+  });
 });
 
-// router.patch('/', (req, res)) {
+router.patch('/',ensureAuthenticated, ensureProfOrTAUser, async (req, res) => {
+  if(!req.body.old){
+    res.status(400).end();
+    return;
+  }
 
-// }
+})
