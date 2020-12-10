2222747617cb8f8636abb1abc3b5f161a83ed66c
Michele Cattaneo <michelecattaneo18@gmail.com>
2020-12-10 22:02:50 +0100

    saving attemp array

--- routes/grades.js
+++ routes/grades.js
@@ -21,7 +21,13 @@ const Attempt = require("../models/Attempt");
 
 module.exports = router;
 
-// appointment_id, Classroom_id, Student_id, TA/Prof_id, mastery_id (take topics from there), array of topic grades, final grade
+/**
+ * Route to add an attempt to a specific masterycheck
+ * @name post/grades?classroom_id=
+ * @function
+ * @param {string} path - Express path
+ * @param {callback} middleware - Express middleware.
+ */
 router.put('/', ensureProfOrTA, (req, res) => {
     Appointment.findById(req.body.appointment_id).then(appointment => {
         // checks appointment belongs to the TA or is a professor
@@ -37,6 +43,7 @@ router.put('/', ensureProfOrTA, (req, res) => {
                         // grades object id for this class
                         let grades_id = user.classrooms_grades.get(req.query.classroom_id);
                         ClassroomGrades.findById(grades_id).then(classroom_grades => {
+                                console.log(classroom_grades);
                                 let new_attempt = new Attempt({
                                     _mastery_id: req.body.mastery_id, 
                                     topic_grades: req.body.topic_grades,
@@ -46,7 +53,6 @@ router.put('/', ensureProfOrTA, (req, res) => {
                                 if (classroom_grades.mastery_grades.get(req.body.mastery_id)) {
                                     console.log("c'e gia");
                                     classroom_grades.mastery_grades.get(req.body.mastery_id).push(new_attempt);
-                                    
                                 }
                                 // if there's not a mapping for this mastery check ( first attempt ) mastery_id => Attempts[]
                                 else {
@@ -55,7 +61,7 @@ router.put('/', ensureProfOrTA, (req, res) => {
                                     classroom_grades.mastery_grades.set(req.body.mastery_id, attempts); 
                                 }
                                 console.log(classroom_grades);
-                                user.save(); // TODO: non va nulla :)
+                                classroom_grades.save();
                                 res.status(200).end();
                             })
                     })
@@ -76,20 +82,6 @@ router.put('/', ensureProfOrTA, (req, res) => {
         })
 })
 
-// const ClassroomGradesSchema = new mongoose.Schema({
-//     _userId: {
-//       type: mongoose.Schema.Types.ObjectId,
-//       required: true,
-//       ref: "User",
-//     },
-//     _classroomId:{
-//         type: mongoose.Schema.Types.ObjectId,
-//         required:true,
-//         ref: 'Classroom'
-//     },
-//     mastery_grades:{ mastery_id -> { topic_map: {map topic_id -> grade}, final_grade: int }
-//       type:Map,
-//       required:true,
-//       default: {}
-//     }
-//   });
+router.get("/all", ensureAuthenticated, (req, res) => {
+
+});
